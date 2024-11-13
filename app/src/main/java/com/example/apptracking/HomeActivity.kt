1daee42f8275bc54a05e071050043e556da76433
Cristhian <cristhianjavierramirezosorio@gmail.com>
2024-11-12 21:26:09 -0500

    add login frontend

--- app/src/main/java/com/example/apptracking/HomeActivity.kt
+++ app/src/main/java/com/example/apptracking/HomeActivity.kt
@@ -1,29 +1,20 @@
 package com.example.apptracking
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btn: Button = findViewById(R.id.loginButton)
-
-        btn.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
         }
     }
 }
